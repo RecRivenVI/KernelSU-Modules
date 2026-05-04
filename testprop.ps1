672fcefed15c6bff7eb162105ef46662766d978a
RavenYin <178878631@qq.com>
2026-05-04 12:28:44 +0800

    更新

---
+++ testprop.ps1
@@ -0,0 +1,3 @@
+& adb -s 624d7620 shell getprop | Select-String 'background_blur'
+& adb -s 624d7620 shell getprop | Select-String 'advanced_visual'
+& adb -s 624d7620 shell getprop | Select-String 'smoothcorner'
